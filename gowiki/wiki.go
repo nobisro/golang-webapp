4908ea78e9c59423f37425f8bcee81042f45dd9e
Rohan Nobis <nobisro@gmail.com>
2018-11-20 18:51:58 -0500

    added edit handling function

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"html/template"
 )
 
 type Page struct {
@@ -35,6 +36,20 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	// Use of (_) to ignore error return value from loadPage
 	p, _ := loadPage(title)
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	fmt.Fprintf(w, "<h1>Edititing %s</h1>" + 
+	"<form action=\"/save/%s\" method=\"POST\">"+
+	"<textarea name=\"body\">%s</textarea><br>"+
+	"<input type=\"submit\" value=\"Save\">"+
+	"</form>",
+	p.Title, p.Title, p.Body)
 }
 
 func main() {
